b8c251878ac3f40bd7f1d9c1f774b84d092b9c71
Beanlord25 <deanmjenvey@gmail.com>
2025-10-12 13:57:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-function currentTime() {
-  const now = new Date();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
-
-  // Add leading zeros for single-digit minutes and seconds
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  seconds = seconds < 10 ? '0' + seconds : seconds;
-
-
-  // You can also update an HTML element:
-  document.getElementById('timeDisplay').textContent = currentTime;
-}
-
-// Call the function to display the time once
-displayCurrentTime();
-
-// To update the time every second, use setInterval
-setInterval(displayCurrentTime, 1000); w
 
 
 var x = document.getElementById("myAudio");
